74926bebd1da3923170aa785461be94b31db6cff
DaveMascia <dave@davemascia.com>
2017-05-30 12:58:49 -0400

    factorial clean up

--- javascript/factorial.js
+++ javascript/factorial.js
@@ -6,13 +6,9 @@ factorial(5) => 5*4*3*2*1 => 120
 "use strict";
 
 const factorial = n => {
-  let sum = n;
+  if (n === 1) { return n; }
 
-  if (n === 1) {
-    return sum;
-  }
-
-  return sum * factorial(n - 1);
+  return n * factorial(n - 1);
 };
 
 console.log(factorial(5) + " should be 120");
